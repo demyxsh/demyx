69f5e57dd0dae226a4ea92468e80f3435fa4d486
cim <info@demyx.com>
2019-04-03 13:57:37 -0700

    logs.sh now checks if an existing log file is actually a directory and changed MariaDB values

--- etc/functions/logs.sh
+++ etc/functions/logs.sh
@@ -3,6 +3,9 @@ source /srv/demyx/etc/.env
 
 DOMAIN=$1
 
-if [ ! -f $LOGS/$DOMAIN.access.log ] && [ ! -f $LOGS/$DOMAIN.error.log ]; then
+if [ -d $LOGS/$DOMAIN.access.log ] && [ -d $LOGS/$DOMAIN.error.log ]; then
+	sudo rm $LOGS/$DOMAIN.access.log $LOGS/$DOMAIN.error.log
+	touch $LOGS/$DOMAIN.access.log $LOGS/$DOMAIN.error.log
+elif [ ! -f $LOGS/$DOMAIN.access.log ] && [ ! -f $LOGS/$DOMAIN.error.log ]; then
 	touch $LOGS/$DOMAIN.access.log $LOGS/$DOMAIN.error.log
 fi
