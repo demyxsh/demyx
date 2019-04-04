0108183de3e1941f8556c5929354305efa4bed6b
cim <info@demyx.com>
2019-04-04 00:45:17 -0700

    Passing -rf flag to rm

--- etc/functions/logs.sh
+++ etc/functions/logs.sh
@@ -4,7 +4,7 @@ source /srv/demyx/etc/.env
 DOMAIN=$1
 
 if [ -d $LOGS/$DOMAIN.access.log ] && [ -d $LOGS/$DOMAIN.error.log ]; then
-	sudo rm $LOGS/$DOMAIN.access.log $LOGS/$DOMAIN.error.log
+	sudo rm -rf $LOGS/$DOMAIN.access.log $LOGS/$DOMAIN.error.log
 	touch $LOGS/$DOMAIN.access.log $LOGS/$DOMAIN.error.log
 elif [ ! -f $LOGS/$DOMAIN.access.log ] && [ ! -f $LOGS/$DOMAIN.error.log ]; then
 	touch $LOGS/$DOMAIN.access.log $LOGS/$DOMAIN.error.log
