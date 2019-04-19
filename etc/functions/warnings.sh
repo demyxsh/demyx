f65dffa2d89998edb01e3a1ce69c3775d33c0ede
cim <info@demyx.com>
2019-04-19 16:18:43 -0700

    Removed docker exec

--- etc/functions/warnings.sh
+++ etc/functions/warnings.sh
@@ -8,7 +8,7 @@ WP_CHECK=$(grep -rs "WP_ID" "$APPS"/"$DOMAIN"/.env)
 
 if [ -n "$WP_CHECK" ]; then
 	source "$APPS"/"$DOMAIN"/.env
-	DEV_MODE_CHECK=$(docker exec -it "$WP" grep -r "sendfile off" /etc/nginx/nginx.conf)
+	DEV_MODE_CHECK=$(grep -r "sendfile off" "$APPS"/"$DOMAIN"/conf/nginx.conf)
 	[[ -n "$DEV_MODE_CHECK" ]] && echo -e "\e[33m[WARNING]\e[39m $DOMAIN is currently in development mode"
 	[[ -d "$APPS"/"$DOMAIN"/db ]] && echo -e "\e[33m[WARNING]\e[39m $DOMAIN is using the old file structure. \n\nPlease run: demyx wp --dom=$DOMAIN --update=structure --ssl \nTo update all old sites: demyx wp --update=structure --all --ssl \n \n\e[31mBACKUP BEFORE YOU RUN THE UPDATER\e[39m \n"
 fi
