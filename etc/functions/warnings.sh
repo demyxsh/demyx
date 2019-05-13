9f40788d48b0fff14106c1c8b291c72f6b9c908f
Cim <cim@demyx.com>
2019-05-12 23:41:23 -0700

    Remove WP_CHECK

--- etc/functions/warnings.sh
+++ etc/functions/warnings.sh
@@ -4,11 +4,10 @@
 
 source /srv/demyx/etc/.env
 DOMAIN=$1
-WP_CHECK=$(grep -rs "WP_ID" "$APPS"/"$DOMAIN"/.env)
 
-if [ -n "$WP_CHECK" ]; then
-    source "$APPS"/"$DOMAIN"/.env
-    DEV_MODE_CHECK=$(grep -s "sendfile off" "$APPS"/"$DOMAIN"/conf/nginx.conf || true)
-    [[ -n "$DEV_MODE_CHECK" ]] && echo -e "\e[33m[WARNING]\e[39m $DOMAIN is currently in development mode"
-    [[ -d "$APPS"/"$DOMAIN"/db ]] && echo -e "\e[33m[WARNING]\e[39m $DOMAIN is using the old file structure. \n\nPlease run: demyx wp --dom=$DOMAIN --update=structure --ssl \nTo update all old sites: demyx wp --update=structure --all --ssl \n \n\e[31mBACKUP BEFORE YOU RUN THE UPDATER\e[39m \n"
-fi
+source "$APPS"/"$DOMAIN"/.env
+DEV_MODE_CHECK=$(grep -s "sendfile off" "$APPS"/"$DOMAIN"/conf/nginx.conf || true)
+[[ -n "$DEV_MODE_CHECK" ]] && echo -e "\e[33m[WARNING]\e[39m $DOMAIN is currently in development mode"
+[[ -d "$APPS"/"$DOMAIN"/db ]] && echo -e "\e[33m[WARNING]\e[39m $DOMAIN is using the old file structure. \n\nPlease run: demyx wp --dom=$DOMAIN --update=structure --ssl \nTo update all old sites: demyx wp --update=structure --all --ssl \n \n\e[31mBACKUP BEFORE YOU RUN THE UPDATER\e[39m \n"
+
+exit 0
