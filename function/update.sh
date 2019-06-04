36b995ca09897666edc0f9b142594c937af86334
Demyx <info@demyx.sh>
2019-06-04 02:29:04 -0700

    Remove update echo

--- function/update.sh
+++ function/update.sh
@@ -8,8 +8,7 @@ function demyx_update() {
     source "$DEMYX_FUNCTION"/env.sh
     source "$DEMYX_FUNCTION"/yml.sh
 
-    demyx_echo 'Updating demyx'
-    demyx_execute -v cd "$DEMYX_ETC" && git pull
+    cd "$DEMYX_ETC" && git pull
 
     demyx_echo 'Updating stack .env'
     demyx_execute demyx_stack_env
