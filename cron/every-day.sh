e6a1b9b2761fa694316bdff9078476f03c206ec5
Demyx <info@demyx.sh>
2019-06-09 11:59:43 +0000

    Added cron updater for MOTD update counter

--- cron/every-day.sh
+++ cron/every-day.sh
@@ -5,7 +5,16 @@
 
 # Rotate demyx log
 /usr/local/bin/demyx log --rotate=demyx
+
 # Rotate stack log
 /usr/local/bin/demyx log --rotate=stack
+
 # Rotate WordPress log
 /usr/local/bin/demyx log --rotate=wp
+
+# Check for Demyx updates
+cd /demyx/etc
+git remote update
+DEMYX_CRON_UPDATES=$(git rev-list HEAD...origin/master --count)
+/bin/sed -i '/DEMYX_MOTD_STATUS/d' /demyx/.env
+echo "DEMYX_MOTD_STATUS=$DEMYX_CRON_UPDATES" >> /demyx/.env
