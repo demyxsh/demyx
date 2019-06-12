63f558efebefebcea2ee26697c9606e91e606899
Demyx <info@demyx.sh>
2019-06-12 08:28:30 +0000

    Using exit code rather than the countless HTTP codes

--- function/healthcheck.sh
+++ function/healthcheck.sh
@@ -10,13 +10,13 @@ function demyx_healthcheck() {
     do
         source "$DEMYX_WP"/"$i"/.env
         
-        DEMYX_HEALTHCHECK_STATUS=$(curl --write-out %{http_code} --silent --output /dev/null --head "$DEMYX_APP_WP_CONTAINER")
+        DEMYX_HEALTHCHECK_STATUS=$(curl -sSf "$DEMYX_APP_WP_CONTAINER" > /dev/null; echo "$?")
 
-        if [[ "$DEMYX_HEALTHCHECK_STATUS" != 200 ]] || [[ "$DEMYX_HEALTHCHECK_STATUS" != 301 ]]; then
+        if [[ "$DEMYX_HEALTHCHECK_STATUS" != 0 ]]; then
             if [[ ! -f "$DEMYX_WP"/"$i"/.healthcheck ]]; then
-                demyx_execute -v echo "DEMYX_APP_HEALTHCHECK_COUNT=0" > "$DEMYX_WP"/"$i"/.healthcheck 
+                demyx_execute -v echo "DEMYX_APP_HEALTHCHECK_COUNT=0" > "$DEMYX_WP"/"$i"/.healthcheck
             fi
-                
+
             source "$DEMYX_WP"/"$i"/.healthcheck
         
             if [[ "$DEMYX_APP_HEALTHCHECK_COUNT" != 3 ]]; then
@@ -25,8 +25,9 @@ function demyx_healthcheck() {
                 demyx compose "$i" du
             else
                 if [[ ! -f "$DEMYX_WP"/"$i"/.healthcheck-lock ]]; then
+                    DEMYX_HEALTHCHECK_HTTP_CODE=$(curl --write-out %{http_code} --silent --output /dev/null --head "$DEMYX_APP_WP_CONTAINER")
                     touch "$DEMYX_WP"/"$i"/.healthcheck-lock
-                    [[ -f "$DEMYX"/custom/callback.sh ]] && demyx_execute -v /bin/bash "$DEMYX"/custom/callback.sh "healthcheck" "$i" "$DEMYX_HEALTHCHECK_STATUS"
+                    [[ -f "$DEMYX"/custom/callback.sh ]] && demyx_execute -v /bin/bash "$DEMYX"/custom/callback.sh "healthcheck" "$i" "$DEMYX_HEALTHCHECK_HTTP_CODE"
                 fi
             fi
         else
