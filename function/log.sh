6b69ea8244af87722c3404ceebb088de9e858b98
Demyx <info@demyx.sh>
2019-06-12 01:04:22 +0000

    Removed dumb-init in the demyx sudo wrapper, causing weird bugs to ET

--- function/log.sh
+++ function/log.sh
@@ -33,7 +33,7 @@ function demyx_log() {
         docker logs "$DEMYX_APP_WP_CONTAINER" "$@"
     else
         if [[ -n "$DEMYX_LOG_FOLLOW" ]]; then
-            docker logs -f demyx
+            tail -f /var/log/demyx/demyx.log
         elif [[ -n "$DEMYX_LOG_ROTATE" ]]; then
             if [[ "$DEMYX_LOG_ROTATE" = demyx ]]; then
                 demyx_echo 'Rotating demyx log'
