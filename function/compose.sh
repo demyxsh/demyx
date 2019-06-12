db2775067809b92b676e7a53684414ffec1abac9
Demyx <info@demyx.sh>
2019-06-12 07:36:35 +0000

    Log compose commands

--- function/compose.sh
+++ function/compose.sh
@@ -17,57 +17,57 @@ function demyx_compose() {
     elif [[ "$DEMYX_APP_TYPE" = wp ]]; then
         if [[ "$DEMYX_COMPOSE" = db ]]; then
             shift
-            docker run -t --rm \
+            demyx_execute -v docker run -t --rm \
             -v /var/run/docker.sock:/var/run/docker.sock:ro \
             --volumes-from demyx \
             --workdir "$DEMYX_APP_PATH" \
             demyx/docker-compose "$@" db_"$DEMYX_APP_ID"
         elif [[ "$DEMYX_COMPOSE" = down ]]; then
-            docker run -t --rm \
+            demyx_execute -v docker run -t --rm \
             -v /var/run/docker.sock:/var/run/docker.sock:ro \
             --volumes-from demyx \
             --workdir "$DEMYX_APP_PATH" \
             demyx/docker-compose stop
 
-            docker run -t --rm \
+            demyx_execute -v docker run -t --rm \
             -v /var/run/docker.sock:/var/run/docker.sock:ro \
             --volumes-from demyx \
             --workdir "$DEMYX_APP_PATH" \
             demyx/docker-compose rm -f
         elif [[ "$DEMYX_COMPOSE" = du ]]; then
-            docker run -t --rm \
+            demyx_execute -v docker run -t --rm \
             -v /var/run/docker.sock:/var/run/docker.sock:ro \
             --volumes-from demyx \
             --workdir "$DEMYX_APP_PATH" \
             demyx/docker-compose stop
 
-            docker run -t --rm \
+            demyx_execute -v docker run -t --rm \
             -v /var/run/docker.sock:/var/run/docker.sock:ro \
             --volumes-from demyx \
             --workdir "$DEMYX_APP_PATH" \
             demyx/docker-compose rm -f
 
-            docker run -t --rm \
+            demyx_execute -v docker run -t --rm \
             -v /var/run/docker.sock:/var/run/docker.sock:ro \
             --volumes-from demyx \
             --workdir "$DEMYX_APP_PATH" \
             demyx/docker-compose up -d --remove-orphans
         elif [[ "$DEMYX_COMPOSE" = wp ]]; then
             shift
-            docker run -t --rm \
+            demyx_execute -v docker run -t --rm \
             -v /var/run/docker.sock:/var/run/docker.sock:ro \
             --volumes-from demyx \
             --workdir "$DEMYX_APP_PATH" \
             demyx/docker-compose "$@" wp_"$DEMYX_APP_ID"
         else
-            docker run -t --rm \
+            demyx_execute -v docker run -t --rm \
             -v /var/run/docker.sock:/var/run/docker.sock:ro \
             --volumes-from demyx \
             --workdir "$DEMYX_APP_PATH" \
             demyx/docker-compose "$@"
         fi
     elif [[ -n "$DEMYX_GET_APP" ]]; then
-        docker run -t --rm \
+        demyx_execute -v docker run -t --rm \
         -v /var/run/docker.sock:/var/run/docker.sock:ro \
         --volumes-from demyx \
         --workdir "$DEMYX_APP"/"$DEMYX_TARGET" \
