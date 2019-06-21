6913382d9fc9a8d7df31358bf65bf7a0dd4bb36b
Demyx <info@demyx.sh>
2019-06-21 11:04:58 +0000

    Remove hours from uptime

--- function/info.sh
+++ function/info.sh
@@ -56,7 +56,7 @@ function demyx_info() {
             "disk_used_percentage": "'$(df -h /demyx | sed '1d' | awk '{print $5}')'",
             "memory_used_percentage": "'$(free | grep Mem | awk '{print $3/$2 * 100.0}' | python -c "print round(float(raw_input()))")'%",
             "memory_free_percentage": "'$(free | grep Mem | awk '{print $4/$2 * 100.0}' | python -c "print round(float(raw_input()))")'%",
-            "uptime": "'$(uptime | awk '{print $1 " " $2 " " $3 " " $4 " " $5 " " $6}' | sed 's/,//g')'",
+            "uptime": "'$(uptime | awk '{print $2 " " $3 " " $4 " " $5 " " $6}' | sed 's/,//g')'",
             "load_average": "'$(cat /proc/loadavg | awk '{print $1 " " $2 " " $3}')'",
             "container_running": "'$DEMYX_INFO_CONTAINER_RUNNING'",
             "container_dead": "'$DEMYX_INFO_CONTAINER_DEAD'"' | sed 's/            /    /g'
