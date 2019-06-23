702768ca61d90753b41121b5699d55116f1a9be8
Demyx <info@demyx.sh>
2019-06-23 07:32:34 +0000

    New flag for demyx info: --json

--- function/info.sh
+++ function/info.sh
@@ -15,6 +15,9 @@ function demyx_info() {
             --filter=)
                 demyx_die '"--filter" cannot be empty'
                 ;;
+            --json)
+                DEMYX_INFO_JSON=1
+                ;;
             --quiet)
                 DEMYX_INFO_QUIET=1
                 ;;
@@ -81,22 +84,41 @@ function demyx_info() {
             DEMYX_INFO_DATA_VOLUME=$(demyx exec "$DEMYX_APP_DOMAIN" bash -c "du -sh /var/www/html" | cut -f1)
             DEMYX_INFO_DB_VOLUME=$(demyx exec "$DEMYX_APP_DOMAIN" db sh -c "du -sh /var/lib/mysql/$WORDPRESS_DB_NAME" | cut -f1)
 
-            PRINT_TABLE="DEMYX^ INFO\n"
-            PRINT_TABLE+="PATH^ $DEMYX_APP_PATH\n"
-            PRINT_TABLE+="WP USER^ $WORDPRESS_USER\n"
-            PRINT_TABLE+="WP PASSWORD^ $WORDPRESS_USER_PASSWORD\n"
-            PRINT_TABLE+="WP CONTAINER^ $DEMYX_APP_WP_CONTAINER\n"
-            PRINT_TABLE+="DB CONTAINER^ $DEMYX_APP_DB_CONTAINER\n"
-            PRINT_TABLE+="WP VOLUME^ $DEMYX_INFO_DATA_VOLUME\n"
-            PRINT_TABLE+="DB VOLUME^ $DEMYX_INFO_DB_VOLUME\n"
-            PRINT_TABLE+="SSL^ $DEMYX_APP_SSL\n"
-            PRINT_TABLE+="CACHE^ $DEMYX_APP_CACHE\n"
-            PRINT_TABLE+="CDN^ $DEMYX_APP_CDN\n"
-            PRINT_TABLE+="AUTH^ $DEMYX_APP_AUTH\n"
-            PRINT_TABLE+="WP AUTH^ $DEMYX_APP_AUTH_WP\n"
-            PRINT_TABLE+="DEV^ $DEMYX_APP_DEV\n"
-            PRINT_TABLE+="HEALTHCHECK^ $DEMYX_APP_HEALTHCHECK"
-            demyx_execute -v demyx_table "$PRINT_TABLE"
+            if [[ -n "$DEMYX_INFO_JSON" ]]; then
+                echo '{
+                    "path": "'$DEMYX_APP_PATH'",
+                    "wp_user": "'$WORDPRESS_USER'",
+                    "wp_password": "'$WORDPRESS_USER_PASSWORD'",
+                    "wp_container": "'$DEMYX_APP_WP_CONTAINER'",
+                    "db_container": "'$DEMYX_APP_DB_CONTAINER'",
+                    "wp_volume": "'$DEMYX_INFO_DATA_VOLUME'",
+                    "db_volume": "'$DEMYX_INFO_DB_VOLUME'",
+                    "ssl": "'$DEMYX_APP_SSL'",
+                    "cache": "'$DEMYX_APP_CACHE'",
+                    "cdn": "'$DEMYX_APP_CDN'",
+                    "auth": "'$DEMYX_APP_AUTH'",
+                    "auth_wp": "'$DEMYX_APP_AUTH_WP'",
+                    "dev": "'$DEMYX_APP_DEV'",
+                    "healthcheck": "'$DEMYX_APP_HEALTHCHECK'"' | sed 's/                    /    /g'
+                echo '}'
+            else
+                PRINT_TABLE="DEMYX^ INFO\n"
+                PRINT_TABLE+="PATH^ $DEMYX_APP_PATH\n"
+                PRINT_TABLE+="WP USER^ $WORDPRESS_USER\n"
+                PRINT_TABLE+="WP PASSWORD^ $WORDPRESS_USER_PASSWORD\n"
+                PRINT_TABLE+="WP CONTAINER^ $DEMYX_APP_WP_CONTAINER\n"
+                PRINT_TABLE+="DB CONTAINER^ $DEMYX_APP_DB_CONTAINER\n"
+                PRINT_TABLE+="WP VOLUME^ $DEMYX_INFO_DATA_VOLUME\n"
+                PRINT_TABLE+="DB VOLUME^ $DEMYX_INFO_DB_VOLUME\n"
+                PRINT_TABLE+="SSL^ $DEMYX_APP_SSL\n"
+                PRINT_TABLE+="CACHE^ $DEMYX_APP_CACHE\n"
+                PRINT_TABLE+="CDN^ $DEMYX_APP_CDN\n"
+                PRINT_TABLE+="AUTH^ $DEMYX_APP_AUTH\n"
+                PRINT_TABLE+="WP AUTH^ $DEMYX_APP_AUTH_WP\n"
+                PRINT_TABLE+="DEV^ $DEMYX_APP_DEV\n"
+                PRINT_TABLE+="HEALTHCHECK^ $DEMYX_APP_HEALTHCHECK"
+                demyx_execute -v demyx_table "$PRINT_TABLE"
+            fi
         fi
     else
         [[ -z "$DEMYX_INFO_QUIET" ]] && demyx_die --not-found
