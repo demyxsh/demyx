794f801bf47f6f7e88be566e8b6b1a7f6e65ca94
Demyx <info@demyx.sh>
2019-07-29 04:22:01 +0000

    Remove any reference to Eternal Terminal

--- function/motd.sh
+++ function/motd.sh
@@ -7,7 +7,6 @@ function demyx_motd() {
         DEMYX_MOTD_MODE=$(echo "$DEMYX_MODE" | tr [a-z] [A-Z] | sed -e 's/\r//g')
         DEMYX_HOST_UPPERCASE=$(hostname | tr [a-z] [A-Z])
         [[ -z "$DEMYX_SSH" ]] && DEMYX_SSH=2222
-        [[ -z "$DEMYX_ET" ]] && DEMYX_ET=2022
         [[ -z "$DEMYX_STATUS" ]] && DEMYX_STATUS=0
 
         cat > /demyx/.env <<-EOF
@@ -16,7 +15,6 @@ function demyx_motd() {
             DEMYX_MOTD_HOST=$DEMYX_HOST_UPPERCASE
             DEMYX_MOTD_USER=DEMYX
             DEMYX_MOTD_SSH=$DEMYX_SSH
-            DEMYX_MOTD_ET=$DEMYX_ET
             DEMYX_MOTD_STATUS=$DEMYX_STATUS
 EOF
         sed -i 's/            //g' /demyx/.env
@@ -44,8 +42,8 @@ EOF
 
         cd "$DEMYX_ETC" || exit
 
-        PRINT_TABLE="DEMYX^ HOST^ USER^ SSH/ETSERVER^ STATUS\n"
-        PRINT_TABLE+="$DEMYX_MOTD_MODE^ $DEMYX_MOTD_HOST^ DEMYX^ $DEMYX_MOTD_SSH/$DEMYX_MOTD_ET^ $DEMYX_MOTD_STATUS"
+        PRINT_TABLE="DEMYX^ HOST^ USER^ SSH^ STATUS\n"
+        PRINT_TABLE+="$DEMYX_MOTD_MODE^ $DEMYX_MOTD_HOST^ DEMYX^ $DEMYX_MOTD_SSH^ $DEMYX_MOTD_STATUS"
         demyx_execute -v demyx_table "$PRINT_TABLE"
         demyx_execute -v echo -e "\nLatest Updates\n--------------"
         demyx_execute -v git --no-pager log -5 --format=format:'- %s %C(white dim)(%ar)%C(reset)'
