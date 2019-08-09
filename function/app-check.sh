310a7b96578b7dbbc3c5e24716ededd5f5fdc254
Demyx <info@demyx.sh>
2019-08-09 21:57:02 +0000

    App check only WP apps for now

--- function/app-check.sh
+++ function/app-check.sh
@@ -1,6 +1,6 @@
 # Demyx
 # https://demyx.sh
 
-if [[ -z "$DEMYX_CONFIG_REFRESH" ]]; then
+if [[ -z "$DEMYX_CONFIG_REFRESH" ]] && [[ "$DEMYX_APP_TYPE" = wp ]]; then
     [[ -z "$DEMYX_APP_COMPOSE_PROJECT" ]] && demyx_die --no-help "This app has an outdated config file, please run: demyx config $DEMYX_APP_DOMAIN --refresh"
 fi
