37b0fb042248f3e75d22c86c74bb83a03cb5ab38
Demyx <info@demyx.sh>
2019-08-20 21:06:32 -0700

    Missing custom weekly cron

--- cron/every-week.sh
+++ cron/every-week.sh
@@ -11,3 +11,8 @@
 
 # Rotate WordPress log
 /usr/local/bin/demyx log --rotate=wp
+
+# Execute custom cron
+if [[ -f /demyx/custom/cron/every-week.sh ]]; then
+    bash /demyx/custom/cron/every-week.sh
+fi
