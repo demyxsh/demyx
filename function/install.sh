d2439c90c92ef9a028807aace7ec5e34f2a47d0d
Demyx <info@demyx.sh>
2019-09-26 22:03:44 -0700

    Update configs for Traefik v2

--- function/install.sh
+++ function/install.sh
@@ -61,43 +61,35 @@ demyx_install() {
 
     DEMYX_STACK_AUTH=$(demyx util --user="$DEMYX_INSTALL_USER" --htpasswd="$DEMYX_INSTALL_PASS" --raw)
 
-    [[ -z "$DEMYX_STACK_RECENT_ERRORS" ]] && DEMYX_STACK_RECENT_ERRORS=100
-    [[ -z "$DEMYX_STACK_DOCKER_WATCH" ]] && DEMYX_STACK_DOCKER_WATCH=true
-    [[ -z "$DEMYX_STACK_DOCKER_EXPOSED_BY_DEFAULT" ]] && DEMYX_STACK_DOCKER_EXPOSED_BY_DEFAULT=false
-    [[ -z "$DEMYX_STACK_ENTRYPOINT_DEFAULTENTRYPOINTS" ]] && DEMYX_STACK_ENTRYPOINT_DEFAULTENTRYPOINTS=http,https
-    [[ -z "$DEMYX_STACK_ACME_ONHOSTRULE" ]] && DEMYX_STACK_ACME_ONHOSTRULE=true
+    [[ -z "$DEMYX_STACK_SERVER_IP" ]] && DEMYX_STACK_SERVER_IP=$(demyx util curl -s https://ipecho.net/plain | sed -e 's/\r//g')
+    [[ -z "$DEMYX_STACK_TRACKER" ]] && DEMYX_STACK_TRACKER=on
+    [[ -z "$DEMYX_STACK_AUTO_UPDATE" ]] && DEMYX_STACK_AUTO_UPDATE=on
+    [[ -z "$DEMYX_STACK_MONITOR" ]] && DEMYX_STACK_MONITOR=on
+    [[ -z "$DEMYX_STACK_HEALTHCHECK" ]] && DEMYX_STACK_HEALTHCHECK=on
     [[ -z "$DEMYX_STACK_ACME_STORAGE" ]] && DEMYX_STACK_ACME_STORAGE=/demyx/acme.json
-    [[ -z "$DEMYX_STACK_ACME_ENTRYPOINT" ]] && DEMYX_STACK_ACME_ENTRYPOINT=https
-    [[ -z "$DEMYX_STACK_ACME_HTTPCHALLENGE_ENTRYPOINT" ]] && DEMYX_STACK_ACME_HTTPCHALLENGE_ENTRYPOINT=http
     [[ -z "$DEMYX_STACK_LOG_LEVEL" ]] && DEMYX_STACK_LOG_LEVEL=INFO
     [[ -z "$DEMYX_STACK_LOG_ACCESS" ]] && DEMYX_STACK_LOG_ACCESS=/var/log/demyx/access.log
     [[ -z "$DEMYX_STACK_LOG_ERROR" ]] && DEMYX_STACK_LOG_ERROR=/var/log/demyx/error.log
 
     cat > "$DEMYX_STACK"/.env <<-EOF
         # AUTO GENERATED
-        DEMYX_STACK_DOMAIN=$DEMYX_INSTALL_DOMAIN
-        DEMYX_STACK_AUTH=$DEMYX_STACK_AUTH
-        DEMYX_STACK_RECENT_ERRORS=$DEMYX_STACK_RECENT_ERRORS
-        DEMYX_STACK_DOCKER_WATCH=$DEMYX_STACK_DOCKER_WATCH
-        DEMYX_STACK_DOCKER_EXPOSED_BY_DEFAULT=$DEMYX_STACK_DOCKER_EXPOSED_BY_DEFAULT
-        DEMYX_STACK_ENTRYPOINT_DEFAULTENTRYPOINTS=$DEMYX_STACK_ENTRYPOINT_DEFAULTENTRYPOINTS
-        DEMYX_STACK_ACME_ONHOSTRULE=$DEMYX_STACK_ACME_ONHOSTRULE
-        DEMYX_STACK_ACME_EMAIL=$DEMYX_INSTALL_EMAIL
+        DEMYX_STACK_SERVER_IP=$DEMYX_STACK_SERVER_IP
+        DEMYX_STACK_TRACKER=$DEMYX_STACK_TRACKER
+        DEMYX_STACK_DOMAIN=$DEMYX_STACK_DOMAIN
+        DEMYX_STACK_AUTH=$DEMYX_PARSE_BASIC_AUTH
+        DEMYX_STACK_AUTO_UPDATE=$DEMYX_STACK_AUTO_UPDATE
+        DEMYX_STACK_MONITOR=$DEMYX_STACK_MONITOR
+        DEMYX_STACK_HEALTHCHECK=$DEMYX_STACK_HEALTHCHECK
+        DEMYX_STACK_ACME_EMAIL=$DEMYX_STACK_ACME_EMAIL
         DEMYX_STACK_ACME_STORAGE=$DEMYX_STACK_ACME_STORAGE
-        DEMYX_STACK_ACME_ENTRYPOINT=$DEMYX_STACK_ACME_ENTRYPOINT
-        DEMYX_STACK_ACME_HTTPCHALLENGE_ENTRYPOINT=$DEMYX_STACK_ACME_HTTPCHALLENGE_ENTRYPOINT
         DEMYX_STACK_LOG_LEVEL=$DEMYX_STACK_LOG_LEVEL
         DEMYX_STACK_LOG_ACCESS=$DEMYX_STACK_LOG_ACCESS
         DEMYX_STACK_LOG_ERROR=$DEMYX_STACK_LOG_ERROR
-        DEMYX_FORCE_STS_HEADER=$DEMYX_FORCE_STS_HEADER
-        DEMYX_STS_SECONDS=$DEMYX_STS_SECONDS
-        DEMYX_STS_INCLUDE_SUBDOMAINS=$DEMYX_STS_INCLUDE_SUBDOMAINS
-        DEMYX_STS_PRELOAD=$DEMYX_STS_PRELOAD
 EOF
     sed -i 's/        //' "$DEMYX_STACK"/.env
 
     demyx_echo 'Creating stack .yml'
-    demyx_execute demyx_stack_yml
+    demyx_execute demyx_stack_v2_yml
 
     demyx_echo 'Creating stack config volume'
     demyx_execute docker volume create demyx_traefik
@@ -105,17 +97,5 @@ EOF
     demyx_echo 'Creating stack log volume'
     demyx_execute docker volume create demyx_traefik_log
 
-    demyx_echo 'Creating stack container'
-    demyx_execute docker run -dt --rm \
-        --name demyx_install_container \
-        -v demyx_traefik:/demyx \
-        demyx/utilities bash
-
-    demyx_echo 'Creating stack acme.json'
-    demyx_execute docker exec -t demyx_install_container sh -c "touch /demyx/acme.json && chmod 600 /demyx/acme.json"
-
-    demyx_echo 'Stopping stack container'
-    demyx_execute docker stop demyx_install_container
-
-    demyx stack up -d --remove-orphans
+    demyx compose stack up -d
 }
