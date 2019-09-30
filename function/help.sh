02ccd43d29a7255ee957fe93f57d3bc274d70495
Demyx <info@demyx.sh>
2019-09-29 18:47:42 -0700

    Must enter container name for ignore to work

--- function/help.sh
+++ function/help.sh
@@ -144,7 +144,7 @@ demyx_help() {
         echo "            ouroboros         Configure Ouroboros"
         echo "            --auto-update     Auto update Demyx core files"
         echo "            --healthcheck     Turns on/off healthcheck globally"
-        echo "            --ignore          Used by Ouroboros to ignore updating images, enter off to disable"
+        echo "            --ignore          Used by Ouroboros to ignore updating images, enter container name or off to disable"
         echo "            --monitor         Turns on/off auto scaling globally"
         echo "            --refresh         Refresh env and yml stack files"
         echo "            --tracker         Pings to demyx.sh server to count active Demyx install"
