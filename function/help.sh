9aa1fd153f6c18fb263f4e9f29dcd9ca08c2b441
Demyx <info@demyx.sh>
2019-10-25 00:27:20 -0700

    Add new flags for demyx config: --dev-cpu, --dev-mem

--- function/help.sh
+++ function/help.sh
@@ -34,6 +34,8 @@ demyx_help() {
         echo "                   --db-cpu         Set the app's DB container CPU usage, --db-cpu=null to remove cap"
         echo "                   --db-mem         Set the app's DB container MEM usage, --db-mem=null to remove cap"
         echo "                   --dev            Turns on/off development mode"
+        echo "                   --dev-cpu        Set the dev app's container CPU usage, --dev-cpu=null to remove cap"
+        echo "                   --dev-mem        Set the dev app's container MEM usage, --dev-mem=null to remove cap"
         echo "                   --files          BrowserSync arg: themes, plugins, or custom path"
         echo "                   --force          Force config"
         echo "                   --healthcheck    Turns on/off healthcheck for WordPress container"
