4b39ac8363ef0120626c37925285f3165cfd8319
Demyx <info@demyx.sh>
2019-11-20 01:01:37 -0800

    Add quotes

--- function/check.sh
+++ function/check.sh
@@ -1,7 +1,7 @@
 # Demyx
 # https://demyx.sh
 
-DEMYX_CHECK_SUDO=$(id -u)
+DEMYX_CHECK_SUDO="$(id -u)"
 
 if [ "$DEMYX_CHECK_SUDO" != 0 ]; then
     echo -e "\e[31m[CRITICAL]\e[39m Demyx must be ran as sudo"
