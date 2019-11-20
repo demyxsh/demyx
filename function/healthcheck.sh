77501a9b74ffa5c334a1ee394713bb09ed517194
Demyx <info@demyx.sh>
2019-11-20 01:40:33 -0800

    Healthcheck the nginx container with backwards compatibility and misc updates

--- function/healthcheck.sh
+++ function/healthcheck.sh
@@ -5,6 +5,8 @@
 #
 demyx_healthcheck() {
     source "$DEMYX_STACK"/.env
+    DEMYX_HEALTHCHECK_CONTAINER="$DEMYX_APP_WP_CONTAINER"
+
     if [[ "$DEMYX_STACK_HEALTHCHECK" = true ]]; then
         cd "$DEMYX_WP" || exit
 
@@ -12,10 +14,10 @@ demyx_healthcheck() {
         do
             if [[ -d "$i" ]]; then
                 source "$DEMYX_WP"/"$i"/.env
-
+                [[ -n "$DEMYX_APP_NX_CONTAINER" ]] && DEMYX_HEALTHCHECK_CONTAINER="$DEMYX_APP_NX_CONTAINER"
                 [[ "$DEMYX_APP_HEALTHCHECK" = false ]] && continue
 
-                DEMYX_HEALTHCHECK_STATUS=$(curl -sSf "$DEMYX_APP_WP_CONTAINER" > /dev/null; echo "$?")
+                DEMYX_HEALTHCHECK_STATUS="$(curl -sSf "$DEMYX_HEALTHCHECK_CONTAINER" > /dev/null; echo "$?")"
 
                 if [[ "$DEMYX_HEALTHCHECK_STATUS" != 0 ]]; then
                     if [[ ! -f "$DEMYX_WP"/"$i"/.healthcheck ]]; then
@@ -25,12 +27,12 @@ demyx_healthcheck() {
                     source "$DEMYX_WP"/"$i"/.healthcheck
 
                     if [[ "$DEMYX_APP_HEALTHCHECK_COUNT" != 3 ]]; then
-                        DEMYX_APP_MONITOR_COUNT_UP=$((DEMYX_APP_HEALTHCHECK_COUNT+1))
-                        demyx_execute -v echo "DEMYX_APP_HEALTHCHECK_COUNT=${DEMYX_APP_MONITOR_COUNT_UP}" > "$DEMYX_WP"/"$i"/.healthcheck
+                        DEMYX_APP_MONITOR_COUNT_UP="$((DEMYX_APP_HEALTHCHECK_COUNT+1))"
+                        demyx_execute -v echo "DEMYX_APP_HEALTHCHECK_COUNT=$DEMYX_APP_MONITOR_COUNT_UP" > "$DEMYX_WP"/"$i"/.healthcheck
                         demyx compose "$i" du
                     else
                         if [[ ! -f "$DEMYX_WP"/"$i"/.healthcheck-lock ]]; then
-                            DEMYX_HEALTHCHECK_HTTP_CODE=$(curl --write-out %{http_code} --silent --output /dev/null --head "$DEMYX_APP_WP_CONTAINER")
+                            DEMYX_HEALTHCHECK_HTTP_CODE="$(curl --write-out %{http_code} --silent --output /dev/null --head "$DEMYX_HEALTHCHECK_CONTAINER")"
                             touch "$DEMYX_WP"/"$i"/.healthcheck-lock
                             [[ -f "$DEMYX"/custom/callback.sh ]] && demyx_execute -v /bin/bash "$DEMYX"/custom/callback.sh "healthcheck" "$i" "$DEMYX_HEALTHCHECK_HTTP_CODE"
                         fi
