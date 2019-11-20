65eca3da9122db309644d76f6264e91efdb6d1c2
Demyx <info@demyx.sh>
2019-11-20 01:28:38 -0800

    Add flag for nginx container, execute as root, and add backwards compatibility

--- function/exec.sh
+++ function/exec.sh
@@ -10,6 +10,12 @@ demyx_exec() {
             db)
                 DEMYX_EXEC_DB=1
                 ;;
+            nx)
+                DEMYX_EXEC_NX=1
+                ;;
+            -r)
+                DEMYX_EXEC_ROOT=1
+                ;;
             -t)
                 DEMYX_EXEC_TTY=1
                 ;;
@@ -36,18 +42,29 @@ demyx_exec() {
         DEMYX_EXEC_FLAG="-it"
     fi
 
+    # Execute as root
+    if [[ -n "$DEMYX_EXEC_ROOT" || -n "$DEMYX_EXEC_DB" ]]; then
+        DEMYX_EXEC_AS="--user=root"
+    else
+        DEMYX_EXEC_AS="--user=demyx"
+        
+        # Will remove this backwards compability in December 2019
+        [[ "$DEMYX_APP_WP_IMAGE" = demyx/nginx-php-wordpress ]] && DEMYX_EXEC_AS="--user=root"
+    fi
+
     if [[ "$DEMYX_TARGET" = all ]]; then
         cd "$DEMYX_WP" || exit
         for i in *
         do
-            demyx exec "$i" "$@"
+            demyx exec "$DEMYX_EXEC_AS" "$i" "$@"
         done
     elif [[ "$DEMYX_APP_TYPE" = wp ]]; then
         DEMYX_EXEC_CONTAINER="$DEMYX_APP_WP_CONTAINER"
         [[ -n "$DEMYX_EXEC_DB" ]] && DEMYX_EXEC_CONTAINER="$DEMYX_APP_DB_CONTAINER"
-        docker exec "$DEMYX_EXEC_FLAG" "$DEMYX_EXEC_CONTAINER" "$@"
+        [[ -n "$DEMYX_EXEC_NX" ]] && DEMYX_EXEC_CONTAINER="$DEMYX_APP_NX_CONTAINER"
+        docker exec "$DEMYX_EXEC_FLAG" "$DEMYX_EXEC_AS" "$DEMYX_EXEC_CONTAINER" "$@"
     elif [[ -n "$DEMYX_GET_APP" ]]; then
-        docker exec "$DEMYX_EXEC_FLAG" "$DEMYX_GET_APP" "$@"
+        docker exec "$DEMYX_EXEC_FLAG" "$DEMYX_EXEC_AS" "$DEMYX_GET_APP" "$@"
     else
         demyx_die --not-found
     fi
