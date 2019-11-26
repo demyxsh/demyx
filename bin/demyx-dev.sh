ea5508640e6c4a9e5e5a237fc5d8f4e9a74dd2a7
Demyx <info@demyx.sh>
2019-11-26 03:44:11 -0800

    Use file instead of .env

--- bin/demyx-dev.sh
+++ bin/demyx-dev.sh
@@ -7,5 +7,5 @@ DEMYX_ROOT_CHECK="$(id -u)"
 if [[ "$DEMYX_ROOT_CHECK" = 0 ]]; then
     find /demyx -type d -print0 | xargs -0 chmod 0755
     find /demyx -type f -print0 | xargs -0 chmod 0644
-    sed -i "s|DEMYX_ENV_MODE=.*|DEMYX_ENV_MODE=development|g" /demyx/.env
+    touch /tmp/demyx-dev
 fi
--- bin/demyx-prod.sh
+++ bin/demyx-prod.sh
@@ -2,18 +2,10 @@
 # Demyx
 # https://demyx.sh
 
-# Generate .env for chroot.sh
-[[ -z "$DEMYX_HOST" ]] && DEMYX_ENV_HOST="$(hostname)"
-[[ -z "$DEMYX_SSH" ]] && DEMYX_ENV_SSH=2222
-[[ -z "$DEMYX_STATUS" ]] && DEMYX_ENV_STATUS=0
+# Delete dev file
+[[ -f /tmp/demyx-dev ]] && rm -f /tmp/demyx-dev
 
-echo "# AUTO GENERATED
-        DEMYX_ENV_MODE=production
-        DEMYX_ENV_HOST=$DEMYX_HOST
-        DEMYX_ENV_USER=demyx
-        DEMYX_ENV_SSH=$DEMYX_SSH
-        DEMYX_ENV_STATUS=$DEMYX_STATUS" | sed 's|        ||g' > /demyx/.env
-
+# Set proper ownership and permission
 chown -R demyx:demyx /home/demyx
 chown -R demyx:demyx /demyx 
 chmod -R a=X /demyx
