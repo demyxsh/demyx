75fbf54dc930afc03ed36df125179cb1b0e640c4
Demyx <info@demyx.sh>
2019-11-29 23:49:37 -0800

    Move temporary code

--- bin/demyx-prod.sh
+++ bin/demyx-prod.sh
@@ -5,10 +5,11 @@
 # Delete dev file
 [[ -f /tmp/demyx-dev ]] && rm -f /tmp/demyx-dev
 
-# Set proper ownership and permission
+# Temporary fix, will remove this soon
 find /var/log/demyx -type d -print0 | xargs -0 chmod 0755
 find /var/log/demyx -type f -print0 | xargs -0 chmod 0644
 
+# Set proper ownership and permission
 chown -R demyx:demyx /demyx
 chown -R demyx:demyx /home/demyx
 chown -R demyx:demyx /var/log/demyx
