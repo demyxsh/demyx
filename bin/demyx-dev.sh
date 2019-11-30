acc1821cbc86e7b80818ea798f6358c1094c90f7
Demyx <info@demyx.sh>
2019-11-29 21:17:34 -0800

    Remove log permission setting

--- bin/demyx-dev.sh
+++ bin/demyx-dev.sh
@@ -7,7 +7,5 @@ DEMYX_ROOT_CHECK="$(id -u)"
 if [[ "$DEMYX_ROOT_CHECK" = 0 ]]; then
     find /demyx -type d -print0 | xargs -0 chmod 0755
     find /demyx -type f -print0 | xargs -0 chmod 0644
-    find /var/log/demyx -type d -print0 | xargs -0 chmod 0755
-    find /var/log/demyx -type f -print0 | xargs -0 chmod 0644
     touch /tmp/demyx-dev
 fi
--- bin/demyx-prod.sh
+++ bin/demyx-prod.sh
@@ -6,8 +6,10 @@
 [[ -f /tmp/demyx-dev ]] && rm -f /tmp/demyx-dev
 
 # Set proper ownership and permission
+find /var/log/demyx -type d -print0 | xargs -0 chmod 0755
+find /var/log/demyx -type f -print0 | xargs -0 chmod 0644
+
 chown -R demyx:demyx /demyx
 chown -R demyx:demyx /home/demyx
 chown -R demyx:demyx /var/log/demyx
 chmod -R a=X /demyx
-chmod -R a=X /var/log/demyx
