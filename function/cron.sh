af4d660ca8b50e26bb033c2460655d275d02ac32
Demyx <info@demyx.sh>
2019-11-29 21:18:49 -0800

    Renamed cron scripts and added temporary backwards compatibility

--- function/cron.sh
+++ function/cron.sh
@@ -66,6 +66,12 @@ demyx_cron() {
         demyx_execute -v git pull
         
         # Execute custom cron
+        if [[ -f /demyx/custom/cron/daily.sh ]]; then
+            echo "[$(date +%F-%T)] CROND: CUSTOM EVERY DAY"
+            demyx_execute -v bash /demyx/custom/cron/daily.sh
+        fi
+
+        # Will remove this backwards compability in January 1st, 2020
         if [[ -f /demyx/custom/cron/every-day.sh ]]; then
             echo "[$(date +%F-%T)] CROND: CUSTOM EVERY DAY"
             demyx_execute -v bash /demyx/custom/cron/every-day.sh
@@ -107,6 +113,12 @@ demyx_cron() {
         demyx_execute -v demyx healthcheck
 
         # Execute custom cron
+        if [[ -f /demyx/custom/cron/minute.sh ]]; then
+            echo "[$(date +%F-%T)] CROND: CUSTOM"
+            demyx_execute -v bash /demyx/custom/cron/minute.sh
+        fi
+
+        # Will remove this backwards compability in January 1st, 2020
         if [[ -f /demyx/custom/cron/every-minute.sh ]]; then
             echo "[$(date +%F-%T)] CROND: CUSTOM"
             demyx_execute -v bash /demyx/custom/cron/every-minute.sh
@@ -124,6 +136,12 @@ demyx_cron() {
         demyx_execute -v -v demyx wp all cron event run --due-now
 
         # Execute custom cron
+        if [[ -f /demyx/custom/cron/six-hour.sh ]]; then
+            echo "[$(date +%F-%T)] CROND: CUSTOM"
+            demyx_execute -v -v bash /demyx/custom/cron/six-hour.sh
+        fi
+
+        # Will remove this backwards compability in January 1st, 2020
         if [[ -f /demyx/custom/cron/every-6-hour.sh ]]; then
             echo "[$(date +%F-%T)] CROND: CUSTOM"
             demyx_execute -v -v bash /demyx/custom/cron/every-6-hour.sh
@@ -142,6 +160,12 @@ demyx_cron() {
         done
 
         # Execute custom cron
+        if [[ -f /demyx/custom/cron/weekly.sh ]]; then
+            echo "[$(date +%F-%T)] CROND: CUSTOM EVERY WEEK"
+            demyx_execute -v bash /demyx/custom/cron/weekly.sh
+        fi
+
+        # Will remove this backwards compability in January 1st, 2020
         if [[ -f /demyx/custom/cron/every-week.sh ]]; then
             echo "[$(date +%F-%T)] CROND: CUSTOM EVERY WEEK"
             demyx_execute -v bash /demyx/custom/cron/every-week.sh
