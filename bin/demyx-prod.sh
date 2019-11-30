cba5cb8a6d4bed4e66de28d0f0fcbeba4522cb02
Demyx <info@demyx.sh>
2019-11-29 19:24:07 -0800

    Add /var/log/demyx when setting prod/dev mode

--- bin/demyx-prod.sh
+++ bin/demyx-prod.sh
@@ -6,6 +6,8 @@
 [[ -f /tmp/demyx-dev ]] && rm -f /tmp/demyx-dev
 
 # Set proper ownership and permission
+chown -R demyx:demyx /demyx
 chown -R demyx:demyx /home/demyx
-chown -R demyx:demyx /demyx 
+chown -R demyx:demyx /var/log/demyx
 chmod -R a=X /demyx
+chmod -R a=X /var/log/demyx
