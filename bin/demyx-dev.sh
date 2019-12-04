9b477f85131fba39b3f9fa813e013553d0f7451c
Demyx <info@demyx.sh>
2019-12-03 17:48:22 -0800

    Already lockdowned by Dockerfile

--- bin/demyx-dev.sh
+++ bin/demyx-dev.sh
@@ -2,10 +2,6 @@
 # Demyx
 # https://demyx.sh
 
-# Run only if user is root
-DEMYX_ROOT_CHECK="$(id -u)"
-if [[ "$DEMYX_ROOT_CHECK" = 0 ]]; then
-    find /demyx -type d -print0 | xargs -0 chmod 0755
-    find /demyx -type f -print0 | xargs -0 chmod 0644
-    touch /tmp/demyx-dev
-fi
+find /demyx -type d -print0 | xargs -0 chmod 0755
+find /demyx -type f -print0 | xargs -0 chmod 0644
+touch /tmp/demyx-dev
