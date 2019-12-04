69494e21f19e4836c109def5d108947a74a43795
Demyx <info@demyx.sh>
2019-12-03 17:49:39 -0800

    Generates docker-compose.yml and .env for demyx and demyx_socket

---
+++ bin/demyx-yml.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+# Demyx
+# https://demyx.sh
+
+# Scan for open ports starting with 2222
+while true; do
+DEMYX_YML_SFTP_OPEN_PORT="$(netstat -tuplen 2>/dev/null | grep :${DEMYX_YML_SSH} || true)"
+    if [[ -z "$DEMYX_YML_SFTP_OPEN_PORT" ]]; then
+        break
+    else
+        DEMYX_YML_SSH="$((DEMYX_YML_SSH+1))"
+    fi
+done
+
+# Check for stack .env
+DEMYX_YML_GET_STACK_ENV="$([[ -f /demyx/app/stack/.env ]] && cat /demyx/app/stack/.env)"
+
+# Set variables
+if [[ -n "$DEMYX_YML_GET_STACK_ENV" ]]; then
+    DEMYX_YML_DOMAIN="$(echo "$DEMYX_YML_GET_STACK_ENV" | grep DEMYX_STACK_SERVER_API | awk -F '[=]' '{print $2}')"
+    DEMYX_YML_AUTH="$(echo "$DEMYX_YML_GET_STACK_ENV" | grep DEMYX_STACK_AUTH | awk -F '[=]' '{print $2}')"
+
+    if [[ "$DEMYX_YML_DOMAIN" ]]; then
+        DEMYX_YML_LABELS="labels:
+      - \"traefik.enable=true\"
+      - \"traefik.http.routers.demyx.rule=Host(\`\$DEMYX_YML_DOMAIN\`)\"
+      - \"traefik.http.routers.demyx.entrypoints=https\"
+      - \"traefik.http.routers.demyx.tls.certresolver=demyx\"
+      - \"traefik.http.routers.demyx.service=demyx\"
+      - \"traefik.http.services.demyx.loadbalancer.server.port=8080\"
+      - \"traefik.http.routers.demyx.middlewares=demyx-auth\"
+      - \"traefik.http.middlewares.demyx-auth.basicauth.users=\${DEMYX_YML_AUTH}\""
+
+        # Generate /demyx/.env
+        echo "# AUTO GENERATED
+        DEMYX_YML_DOMAIN="$DEMYX_YML_DOMAIN"
+        DEMYX_YML_AUTH="$DEMYX_YML_AUTH"
+        " | sed "s|        ||g" > /demyx/.env
+    fi
+fi
+
+# Generate /demyx/docker-compose.yml
+echo "# AUTO GENERATED
+version: \"2.4\"
+services:
+  socket:
+    privileged: true
+    image: demyx/docker-socket-proxy
+    cpus: ${DEMYX_YML_CPU}
+    mem_limit: ${DEMYX_YML_MEM}
+    container_name: demyx_socket
+    restart: unless-stopped
+    networks:
+      - demyx
+      - demyx_socket
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock:ro
+    environment:
+      - CONTAINERS=1
+      - EXEC=1
+      - IMAGES=1
+      - INFO=1
+      - NETWORKS=1
+      - POST=1
+      - VOLUMES=1
+  demyx:
+    image: demyx/demyx
+    cpus: ${DEMYX_YML_CPU}
+    mem_limit: ${DEMYX_YML_MEM}
+    container_name: demyx
+    restart: unless-stopped
+    hostname: ${DEMYX_YML_HOST}
+    depends_on: 
+      - socket
+    networks:
+      - demyx
+      - demyx_socket
+    volumes:
+      - demyx:/demyx
+      - demyx_user:/home/demyx
+      - demyx_log:/var/log/demyx
+    environment:
+      - DOCKER_HOST=tcp://demyx_socket:2375
+      - DEMYX_BRANCH="$DEMYX_YML_BRANCH"
+      - DEMYX_MODE="$DEMYX_YML_MODE"
+      - DEMYX_HOST="$DEMYX_YML_HOST"
+      - DEMYX_SSH="$DEMYX_YML_SSH"
+      - TZ=America/Los_Angeles
+    ports:
+      - ${DEMYX_YML_SSH}:2222
+    $DEMYX_YML_LABELS
+volumes:
+  demyx:
+    name: demyx
+  demyx_log:
+    name: demyx_log
+  demyx_user:
+    name: demyx_user
+networks:
+  demyx:
+    name: demyx
+  demyx_socket:
+    name: demyx_socket
+" > /demyx/docker-compose.yml
