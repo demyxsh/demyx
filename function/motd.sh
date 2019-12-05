7bbe8c1e81c5b255d8cdce1d5ee3452201dca61d
Demyx <info@demyx.sh>
2019-12-04 22:49:47 -0800

    Use demyx info system for better performance

--- function/motd.sh
+++ function/motd.sh
@@ -86,40 +86,7 @@ demyx_motd() {
         demyx_execute -v echo -e '\e[31m==========[BREAKING CHANGES]==========\e[39m\n\nFor best security practice and performance, all demyx containers will now\nrun as the demyx user, including the WordPress containers. Each WordPress\nsites will now have a total of 3 containers: MariaDB, NGINX, and WordPress.\nCertain demyx commands will not work until you upgrade the sites.\n\nPlease run the following commands:\n'
         demyx_upgrade_apps
     else
-        DEMYX_MOTD_MODE="$(demyx_get_mode)"
-        DEMYX_MOTD_BACKUPS="$([[ -d "$DEMYX_BACKUP_WP" ]] && du -sh "$DEMYX_BACKUP_WP" | cut -f1)"
-        [[ -z "$DEMYX_MOTD_BACKUPS" ]] && DEMYX_MOTD_BACKUPS=0
-        DEMYX_MOTD_SYSTEM_INFO="$(demyx info system --json)"
-        DEMYX_MOTD_SYSTEM_DISK="$(echo "$DEMYX_MOTD_SYSTEM_INFO" | jq .disk_used | sed 's|"||g')"
-        DEMYX_MOTD_SYSTEM_DISK_TOTAL="$(echo "$DEMYX_MOTD_SYSTEM_INFO" | jq .disk_total | sed 's|"||g')"
-        DEMYX_MOTD_SYSTEM_DISK_TOTAL_PERCENTAGE="$(echo "$DEMYX_MOTD_SYSTEM_INFO" | jq .disk_total_percentage | sed 's|"||g')"
-        DEMYX_MOTD_SYSTEM_DISK_TOTAL_PERCENTAGE_NUMERIC="$(echo "$DEMYX_MOTD_SYSTEM_DISK_TOTAL_PERCENTAGE" | sed "s|%||g")"
-        DEMYX_MOTD_SYSTEM_MEMORY="$(echo "$DEMYX_MOTD_SYSTEM_INFO" | jq .memory_used | sed 's|"||g')"
-        DEMYX_MOTD_SYSTEM_MEMORY_TOTAL="$(echo "$DEMYX_MOTD_SYSTEM_INFO" | jq .memory_total | sed 's|"||g')"
-        DEMYX_MOTD_SYSTEM_UPTIME="$(echo "$DEMYX_MOTD_SYSTEM_INFO" | jq .uptime | sed 's|"||g')"
-        DEMYX_MOTD_SYSTEM_LOAD="$(echo "$DEMYX_MOTD_SYSTEM_INFO" | jq .load_average | sed 's|"||g')"
-        DEMYX_MOTD_SYSTEM_CONTAINER="$(echo "$DEMYX_MOTD_SYSTEM_INFO" | jq .container_running | sed 's|"||g')"
-        DEMYX_MOTD_SYSTEM_CONTAINER_DEAD="$(echo "$DEMYX_MOTD_SYSTEM_INFO" | jq .container_dead | sed 's|"||g')"
-        DEMYX_MOTD_GET_RECENT_MODIFIED="$(find "$DEMYX_ETC" -type f -mtime -1 | xargs ls -lt 2>/dev/null | head -1 | awk '{print $NF}')"
-
-        if [[ "$DEMYX_MOTD_SYSTEM_CONTAINER_DEAD" = 0 ]]; then
-            DEMYX_MOTD_SYSTEM_CONTAINER_DEAD_COUNT=
-        else
-            DEMYX_MOTD_SYSTEM_CONTAINER_DEAD_COUNT="($DEMYX_MOTD_SYSTEM_CONTAINER_DEAD dead)"
-        fi
-
-        PRINT_MOTD_TABLE="DEMYX^ SYSTEM INFO\n"
-        PRINT_MOTD_TABLE+="UPDATED^ $(stat -c '%y' "$DEMYX_MOTD_GET_RECENT_MODIFIED" | awk -F '[.]' '{print $1}')\n"
-        PRINT_MOTD_TABLE+="MODE^ $(echo "$DEMYX_MOTD_MODE" | tr [a-z] [A-Z])\n"
-        PRINT_MOTD_TABLE+="HOST^ $DEMYX_HOST\n"
-        PRINT_MOTD_TABLE+="SSH^ $DEMYX_SSH\n"
-        PRINT_MOTD_TABLE+="BACKUPS^ $DEMYX_MOTD_BACKUPS\n"
-        PRINT_MOTD_TABLE+="DISK^ $DEMYX_MOTD_SYSTEM_DISK/$DEMYX_MOTD_SYSTEM_DISK_TOTAL ($DEMYX_MOTD_SYSTEM_DISK_TOTAL_PERCENTAGE)\n"
-        PRINT_MOTD_TABLE+="MEMORY^ $DEMYX_MOTD_SYSTEM_MEMORY/$DEMYX_MOTD_SYSTEM_MEMORY_TOTAL\n"
-        PRINT_MOTD_TABLE+="UPTIME^ ${DEMYX_MOTD_SYSTEM_UPTIME}\n"
-        PRINT_MOTD_TABLE+="LOAD^ $DEMYX_MOTD_SYSTEM_LOAD\n"
-        PRINT_MOTD_TABLE+="CONTAINERS^ $DEMYX_MOTD_SYSTEM_CONTAINER $DEMYX_MOTD_SYSTEM_CONTAINER_DEAD_COUNT"
-        demyx_execute -v demyx_table "$PRINT_MOTD_TABLE"
+        demyx info system
         echo
     fi
     demyx_motd_yml_check
