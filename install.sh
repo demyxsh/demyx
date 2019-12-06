d8350c2166d3ab756bc9b3aca1b6613641d9ba24
Demyx <info@demyx.sh>
2019-12-06 04:30:57 -0800

    Revert scripts back to bash, needs more testing

--- install.sh
+++ install.sh
@@ -1,25 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 # Demyx
 # https://demyx.sh
-set -eu pipefail
 
-# Set default variables
-DEMYX_INSTALL_SKIP_CHECKS=
 DEMYX_DOCKER_CHECK="$(which docker)"
 DEMYX_SUDO_CHECK="$(id -u)"
 
-if [ "$DEMYX_SUDO_CHECK" != 0 ]; then
+if [[ "$DEMYX_SUDO_CHECK" != 0 ]]; then
     echo -e "\e[31m[CRITICAL]\e[39m Must be ran as root or sudo"
     exit 1
 fi
 
-if [ ! -f "$DEMYX_DOCKER_CHECK" ]; then
+if [[ ! -f "$DEMYX_DOCKER_CHECK" ]]; then
     echo -e "\e[31m[CRITICAL]\e[39m Docker must be installed"
     exit 1
 fi
 
 while :; do
-    case "${1:-}" in
+    case "$1" in
         --skip-checks)
             DEMYX_INSTALL_SKIP_CHECKS=1
             ;;
@@ -37,7 +34,7 @@ while :; do
     shift
 done
 
-if [ -z "$DEMYX_INSTALL_SKIP_CHECKS" ]; then
+if [[ -z "$DEMYX_INSTALL_SKIP_CHECKS" ]]; then
     docker pull demyx/browsersync
     docker pull demyx/code-server:wp
     docker pull demyx/demyx
@@ -55,45 +52,45 @@ if [ -z "$DEMYX_INSTALL_SKIP_CHECKS" ]; then
     docker pull phpmyadmin/phpmyadmin
 fi
 
-printf "\e[34m[INFO]\e[39m Enter top level domain for Traefik dashboard\n"
-read -rp "Domain: " DEMYX_INSTALL_DOMAIN
-if [ -z "$DEMYX_INSTALL_DOMAIN" ]; then
+echo -e "\e[34m[INFO]\e[39m Enter top level domain for Traefik dashboard"
+read -rep "Domain: " DEMYX_INSTALL_DOMAIN
+if [[ -z "$DEMYX_INSTALL_DOMAIN" ]]; then
     echo -e "\e[31m[CRITICAL]\e[39m Domain cannot be empty"
     exit 1
 fi
 
-printf "\e[34m[INFO\e[39m] Lets Encrypt SSL notifications\n"
-read -rp "Email: " DEMYX_INSTALL_EMAIL
-if [ -z "$DEMYX_INSTALL_EMAIL" ]; then
+echo -e "\e[34m[INFO\e[39m] Lets Encrypt SSL notifications"
+read -rep "Email: " DEMYX_INSTALL_EMAIL
+if [[ -z "$DEMYX_INSTALL_EMAIL" ]]; then
     echo -e "\e[31m[CRITICAL]\e[39m Email cannot be empty"
     exit 1
 fi
 
-printf "\e[34m[INFO]\e[39m Enter username for basic auth\n"
-read -rp "Username: " DEMYX_INSTALL_USER
-if [ -z "$DEMYX_INSTALL_USER" ]; then
+echo -e "\e[34m[INFO]\e[39m Enter username for basic auth"
+read -rep "Username: " DEMYX_INSTALL_USER
+if [[ -z "$DEMYX_INSTALL_USER" ]]; then
     echo -e "\e[31m[CRITICAL]\e[39m Username cannot be empty"
     exit 1
 fi
 
-printf "\e[34m[INFO]\e[39m Enter password for basic auth\n"
-read -rp "Password: " DEMYX_INSTALL_PASS
-if [ -z "$DEMYX_INSTALL_PASS" ]; then
+echo -e "\e[34m[INFO]\e[39m Enter password for basic auth"
+read -rep "Password: " DEMYX_INSTALL_PASS
+if [[ -z "$DEMYX_INSTALL_PASS" ]]; then
     echo -e "\e[31m[CRITICAL]\e[39m Password cannot be empty"
     exit 1
 fi
 
-printf "\e[34m[INFO\e[39m] Installing demyx chroot\n"
+echo -e "\e[34m[INFO\e[39m] Installing demyx chroot"
 docker run -t --user=root --privileged --rm -v /usr/local/bin:/usr/local/bin demyx/utilities demyx-chroot
 
 demyx --nc
 
-printf "\e[34m[INFO\e[39m] Waiting for demyx container to initialize\n"
+echo -e "\e[34m[INFO\e[39m] Waiting for demyx container to initialize"
 sleep 5
 demyx cmd install --domain="$DEMYX_INSTALL_DOMAIN" --email="$DEMYX_INSTALL_EMAIL" --user="$DEMYX_INSTALL_USER" --pass="$DEMYX_INSTALL_PASS"
 
-printf "\e[34m[INFO\e[39m] Pinging active server to demyx\n"
+echo -e "\e[34m[INFO\e[39m] Pinging active server to demyx"
 docker run -t --rm demyx/utilities curl -s "https://demyx.sh/?action=active&token=V1VpdGNPcWNDVlZSUDFQdFBaR0Zhdz09OjrnA1h6ZbDFJ2T6MHOwg3p4" -o /dev/null
 
-printf "\e[34m[INFO\e[39m] To SSH into the demyx container, paste your keys in /home/demyx/.ssh/authorized_keys inside the demyx container. Then run on the host OS: demyx restart\n"
+echo -e "\e[34m[INFO\e[39m] To SSH into the demyx container, paste your keys in /home/demyx/.ssh/authorized_keys inside the demyx container. Then run on the host OS: demyx restart"
 demyx restart
