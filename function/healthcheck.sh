cf5bea64a4bb65e45bfb91b08858f7a08c6b050f
Demyx <info@demyx.sh>
2019-12-05 17:28:34 -0800

    Use wget to get container health and curl only for http code

--- function/healthcheck.sh
+++ function/healthcheck.sh
@@ -17,9 +17,9 @@ demyx_healthcheck() {
                 [[ -n "$DEMYX_APP_NX_CONTAINER" ]] && DEMYX_HEALTHCHECK_CONTAINER="$DEMYX_APP_NX_CONTAINER"
                 [[ "$DEMYX_APP_HEALTHCHECK" = false ]] && continue
 
-                DEMYX_HEALTHCHECK_STATUS="$(curl -sL -o /dev/null -w %{http_code} "$DEMYX_HEALTHCHECK_CONTAINER")"
+                DEMYX_HEALTHCHECK_STATUS="$(wget --quiet --tries=1 --spider --timeout=1 "$DEMYX_HEALTHCHECK_CONTAINER"; echo $?)"
 
-                if [[ "$DEMYX_HEALTHCHECK_STATUS" != 200 ]]; then
+                if [[ "$DEMYX_HEALTHCHECK_STATUS" != 0 ]]; then
                     if [[ ! -f "$DEMYX_WP"/"$i"/.healthcheck ]]; then
                         demyx_execute -v echo "DEMYX_APP_HEALTHCHECK_COUNT=0" > "$DEMYX_WP"/"$i"/.healthcheck
                     fi
@@ -35,9 +35,10 @@ demyx_healthcheck() {
                             DEMYX_HEALTHCHECK_SERVER_IP="$(demyx info stack --filter=DEMYX_STACK_SERVER_IP)"
                             DEMYX_HEALTHCHECK_IP="$(dig +short "$DEMYX_APP_DOMAIN" | tr '\r\n' ' ')"
                             DEMYX_HEALTHCHECK_NS="$(dig +short NS "$DEMYX_APP_DOMAIN" | tr '\r\n' ' ')"
+                            DEMYX_HEALTHCHECK_HTTP_STATUS="$(curl -sL -o /dev/null -w %{http_code} "$DEMYX_HEALTHCHECK_CONTAINER")"
                             touch "$DEMYX_WP"/"$i"/.healthcheck-lock
                             if [[ -f "$DEMYX"/custom/callback.sh ]]; then
-                                bash "$DEMYX"/custom/callback.sh "healthcheck" "$i" "$DEMYX_HEALTHCHECK_STATUS" "$DEMYX_HEALTHCHECK_SERVER_IP" "$DEMYX_HEALTHCHECK_IP" "$DEMYX_HEALTHCHECK_NS"
+                                bash "$DEMYX"/custom/callback.sh "healthcheck" "$i" "$DEMYX_HEALTHCHECK_HTTP_STATUS" "$DEMYX_HEALTHCHECK_SERVER_IP" "$DEMYX_HEALTHCHECK_IP" "$DEMYX_HEALTHCHECK_NS"
                             fi
                         fi
                     fi
