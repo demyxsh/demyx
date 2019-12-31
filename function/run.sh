506f3b3884ac5ccdd8d322cd74146e528c8dbab8
Demyx <info@demyx.sh>
2019-12-30 19:53:05 -0800

    Fixed demyx run --clone issue

--- function/run.sh
+++ function/run.sh
@@ -201,11 +201,11 @@ demyx_run() {
                 -v wp_"$DEMYX_APP_ID":"$DEMYX_GLOBAL_WP_VOLUME" \
                 demyx/utilities sh
                 
-            demyx_echo 'Copying files' 
-            demyx_execute docker cp "$DEMYX_APP_PATH"/html "$DEMYX_APP_ID":/var/www
-
             demyx_echo 'Removing old wp-config.php'
-            demyx_execute docker exec -t "$DEMYX_APP_ID" rm "$DEMYX_GLOBAL_WP_VOLUME"/wp-config.php
+            demyx_execute rm -f "$DEMYX_APP_PATH"/demyx/wp-config.php
+
+            demyx_echo 'Copying files' 
+            demyx_execute docker cp "$DEMYX_APP_PATH"/demyx "$DEMYX_APP_ID":/
 
             demyx_echo 'Stopping temporary container'
             demyx_execute docker stop "$DEMYX_APP_ID"
@@ -286,7 +286,7 @@ demyx_run() {
             demyx_execute docker exec -t "$DEMYX_APP_WP_CONTAINER" rm clone.sql
 
             demyx_echo 'Cleaning up'
-            demyx_execute rm -rf "$DEMYX_APP_PATH"/html
+            demyx_execute rm -rf "$DEMYX_APP_PATH"/demyx
 
             demyx config "$DEMYX_APP_DOMAIN" --refresh --no-backup
         elif [[ -n "$DEMYX_RUN_ARCHIVE" ]]; then
