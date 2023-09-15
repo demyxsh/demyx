d0004d54e9ce6c999cdc616db7b7cbaf09f38c94
Demyx <info@demyx.sh>
2023-09-15 12:24:03 -0700

    Merged, moved, or and/deleted

--- bin/demyx-skel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Demyx
-# https://demyx.sh
-
-if [[ ! -d "$DEMYX_WP" ]]; then
-    cp -r "$DEMYX_CONFIG"/skel/. "$DEMYX"
-fi
-
-[[ ! -L "$DEMYX"/log ]] && ln -s "$DEMYX_LOG" "$DEMYX"/log
