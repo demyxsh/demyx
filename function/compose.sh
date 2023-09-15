9fb4848b1b1afcf8fe622bd531673bad13c989fa
Demyx <info@demyx.sh>
2023-09-15 12:53:33 -0700

    demyx_compose_app() - Update logic using case statement - New way to specific containers using flags - Put docker-compose commands in a single variable to prevent repitition - docker-compose stdout will be logged

--- function/compose.sh
+++ function/compose.sh
@@ -29,140 +29,60 @@ demyx_compose_all() {
         eval demyx_compose "$DEMYX_COMPOSE_ALL" "$DEMYX_COMPOSE_ARGS"
     done
 }
+#
+#   Main docker-compose function.
+#
+demyx_compose_app() {
+    local DEMYX_COMPOSE_APP=
+    local DEMYX_COMPOSE_APP_CHECK=
 
-            demyx compose "$i" "$@"
+    DEMYX_COMPOSE_APP_CHECK="$(demyx_app_path "$DEMYX_ARG_2")"
 
-            if [[ "$DEMYX_COMPOSE_CHECK_DB" = --check-db ]]; then
-                DEMYX_COMPOSE_CHECK_DB_CONTAINER="$(docker inspect --format='{{.State.Status}}' "$DEMYX_APP_DB_CONTAINER")"
-
-                if [[ "$DEMYX_COMPOSE_CHECK_DB_CONTAINER" != running ]]; then
-                    demyx config "$i" --fix-innodb
-                fi
-            fi
-        done
-    elif [[ "$DEMYX_APP_TYPE" = wp ]]; then
-        [[ ! -d "$DEMYX_WP"/"$DEMYX_APP_DOMAIN" ]] && demyx_die --not-found
-        shift
-        DEMYX_COMPOSE="$1"
-        if [[ "$DEMYX_COMPOSE" = db ]]; then
-            shift
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$DEMYX_APP_PATH" \
-            demyx/docker-compose "$@" db_"$DEMYX_APP_ID"
-        elif [[ "$DEMYX_COMPOSE" = down ]]; then
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$DEMYX_APP_PATH" \
-            demyx/docker-compose stop
-
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$DEMYX_APP_PATH" \
-            demyx/docker-compose rm -f
-        elif [[ "$DEMYX_COMPOSE" = fr ]]; then
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$DEMYX_APP_PATH" \
-            demyx/docker-compose up -d --force-recreate --remove-orphans
-        elif [[ "$DEMYX_COMPOSE" = nx ]]; then
-            shift
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$DEMYX_APP_PATH" \
-            demyx/docker-compose "$@" nx_"$DEMYX_APP_ID"
-        elif [[ "$DEMYX_COMPOSE" = wp ]]; then
-            shift
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$DEMYX_APP_PATH" \
-            demyx/docker-compose "$@" wp_"$DEMYX_APP_ID"
-        else
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$DEMYX_APP_PATH" \
-            demyx/docker-compose "$@"
-        fi
-    elif [[ -n "$DEMYX_GET_APP" ]]; then
-        [[ ! -d "$DEMYX_APP"/"$DEMYX_TARGET" ]] && demyx_die --not-found
-        shift 1
-        DEMYX_COMPOSE="$1"
-        if [[ "$DEMYX_COMPOSE" = down ]]; then
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$DEMYX_APP"/"$DEMYX_TARGET" \
-            demyx/docker-compose stop
-
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$DEMYX_APP"/"$DEMYX_TARGET" \
-            demyx/docker-compose rm -f
-        elif [[ "$DEMYX_COMPOSE" = fr ]]; then
-            shift
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$DEMYX_APP"/"$DEMYX_TARGET" \
-            demyx/docker-compose up -d --force-recreate --remove-orphans
-        else
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$DEMYX_APP"/"$DEMYX_TARGET" \
-            demyx/docker-compose "$@"
-        fi
-    elif [[ -z "$DEMYX_GET_APP" ]]; then
-        [[ ! -d "$DEMYX_APP"/"$DEMYX_TARGET" ]] && demyx_die --not-found
-        if [[ "$DEMYX_COMPOSE" = down ]]; then
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$PWD" \
-            demyx/docker-compose stop
-
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$PWD" \
-            demyx/docker-compose rm -f
-        elif [[ "$DEMYX_COMPOSE" = fr ]]; then
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$PWD" \
-            demyx/docker-compose up -d --force-recreate --remove-orphans
-        else
-            demyx_execute -v docker run -t --rm \
-            -e DOCKER_HOST=tcp://demyx_socket:2375 \
-            --network=demyx_socket \
-            --volumes-from=demyx \
-            --workdir="$PWD" \
-            demyx/docker-compose "$@"
-        fi
+    if [[ -n "$DEMYX_COMPOSE_APP_CHECK" && "$DEMYX_COMPOSE_APP_CHECK" == *"wp/${DEMYX_ARG_2}"* ]]; then
+        demyx_app_env wp "
+            DEMYX_APP_DOMAIN
+            DEMYX_APP_ID
+            DEMYX_APP_PATH
+        "
+    elif [[ -n "$DEMYX_COMPOSE_APP_CHECK" ]]; then
+        # shellcheck disable=2034
+        DEMYX_APP_PATH="$DEMYX_COMPOSE_APP_CHECK"
     else
-        demyx_die --not-found
+        demyx_error app
+    fi
+
+    cd "$DEMYX_APP_PATH" || exit
+
+    case "$DEMYX_COMPOSE_TYPE" in
+        -d)
+            DEMYX_COMPOSE_APP="docker-compose ${DEMYX_COMPOSE_ARGS//db /} db_${DEMYX_APP_ID}"
+            DEMYX_COMPOSE_APP="${DEMYX_COMPOSE_APP//docker-compose -d/docker-compose}"
+            DEMYX_COMPOSE_APP="${DEMYX_COMPOSE_APP//docker-compose --database/docker-compose}"
+        ;;
+        down)
+            DEMYX_COMPOSE_APP="docker-compose stop && docker-compose rm -f"
+        ;;
+        fr)
+            DEMYX_COMPOSE_APP="docker-compose up -d --force-recreate --remove-orphans"
+        ;;
+        -n)
+            DEMYX_COMPOSE_APP="docker-compose --no-deps ${DEMYX_COMPOSE_ARGS//nx /} nx_${DEMYX_APP_ID}"
+            DEMYX_COMPOSE_APP="${DEMYX_COMPOSE_APP//docker-compose --no-deps -n/docker-compose}"
+            DEMYX_COMPOSE_APP="${DEMYX_COMPOSE_APP//docker-compose --no-deps --nginx/docker-compose}"
+        ;;
+        -w)
+            DEMYX_COMPOSE_APP="docker-compose --no-deps ${DEMYX_COMPOSE_ARGS//wp /} --no-deps wp_${DEMYX_APP_ID}"
+            DEMYX_COMPOSE_APP="${DEMYX_COMPOSE_APP//docker-compose --no-deps -w/docker-compose}"
+            DEMYX_COMPOSE_APP="${DEMYX_COMPOSE_APP//docker-compose --no-deps --wordpress/docker-compose}"
+        ;;
+        *)
+            DEMYX_COMPOSE_APP="docker-compose $DEMYX_COMPOSE_ARGS"
+        ;;
+    esac
+
+    if eval "$DEMYX_COMPOSE_APP" 2>&1 | tee "$DEMYX_TMP"/demyx_execute; then
+        demyx_logger false "$DEMYX_COMPOSE_APP" "$(cat < "$DEMYX_TMP"/demyx_execute)"
+    else
+        demyx_logger false "$DEMYX_COMPOSE_APP" "$(cat < "$DEMYX_TMP"/demyx_execute)" error
     fi
 }
