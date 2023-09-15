e72df8681f9e41394d2f1bbec3ffaa8240b80e08
Demyx <info@demyx.sh>
2023-09-15 15:19:44 -0700

    demyx_images() - Outputs docker images command to file for performance reasons - Uses case statement for specific purposes

--- function/global.sh
+++ function/global.sh
@@ -333,28 +333,32 @@ demyx_execute() {
         exit 1
     fi
 }
-demyx_update_local() {
-    echo "DEMYX_LOCAL_VERSION=$DEMYX_VERSION
-    DEMYX_LOCAL_CODE_VERSION=$(docker run --rm --entrypoint=code-server demyx/code-server:browse --version 2>&1 | awk -F '[ ]' '{print $1}' | awk '{line=$0} END{print line}' | sed 's/\r//g')
-    DEMYX_LOCAL_DOCKER_COMPOSE_VERSION=$(docker run --rm --entrypoint=docker-compose demyx/docker-compose --version 2>&1 | awk -F '[ ]' '{print $3}' | cut -c -6 | sed 's/\r//g')
-    DEMYX_LOCAL_HAPROXY_VERSION=$(docker run --rm --user=root --entrypoint=haproxy demyx/docker-socket-proxy -v 2>&1 | head -1 | awk '{print $3}' | sed 's/\r//g')
-    DEMYX_LOCAL_LOGROTATE_VERSION=$(docker run --rm --entrypoint=logrotate demyx/logrotate --version 2>&1 | head -n 1 | awk -F '[ ]' '{print $2}' | sed 's/\r//g')
-    DEMYX_LOCAL_MARIADB_VERSION=$(docker run --rm --entrypoint=mariadb demyx/mariadb --version 2>&1 | awk -F '[ ]' '{print $6}' | awk -F '[,]' '{print $1}' | sed 's/-MariaDB//g' | sed 's/\r//g')
-    DEMYX_LOCAL_NGINX_VERSION=$(docker run --rm --entrypoint=nginx demyx/nginx -V 2>&1 > /dev/null | head -n 1 | cut -c 22- | sed 's/\r//g')
-    DEMYX_LOCAL_TRAEFIK_VERSION=$(docker run --rm --user=root --entrypoint=traefik demyx/traefik version 2>&1 | sed -n 1p | awk '{print $2}' | sed 's/\r//g')
-    DEMYX_LOCAL_UTILITIES_VERSION=$(docker run --rm demyx/utilities cat /etc/debian_version 2>&1 | sed 's/\r//g')
-    DEMYX_LOCAL_WORDPRESS_VERSION=$(docker run --rm --entrypoint=sh demyx/wordpress -c "grep '\$wp_version =' /demyx/wp-includes/version.php | cut -d\"'\" -f 2" 2>&1 | sed 's/\r//g')
-    DEMYX_LOCAL_WORDPRESS_BEDROCK_VERSION=$(curl -sL https://api.github.com/repos/roots/bedrock/releases/latest 2>&1 | jq -r '.tag_name' | sed 's/\r//g')
-    DEMYX_LOCAL_WORDPRESS_CLI_VERSION=$(docker run --rm demyx/wordpress:cli --version 2>&1 | awk -F '[ ]' '{print $2}' | sed 's/\r//g')
-    DEMYX_LOCAL_WORDPRESS_PHP_VERSION=$(docker run --rm --entrypoint=php demyx/wordpress -v 2>&1 | grep cli | awk -F '[ ]' '{print $2}' | sed 's/\r//g')" | sed "s|    ||g" > "$DEMYX"/.update_local
+#
+#   Output `docker images` to a file for performance and outputs contents to stdout.
+#
+demyx_images() {
+    local DEMYX_IMAGES="${1:-}"
+    local DEMYX_IMAGES_FILE="$DEMYX"/.images
 
-    if [[ -n "$(docker images demyx/browsersync:latest -q)" ]]; then
-        echo "DEMYX_LOCAL_BROWSERSYNC_VERSION=$(docker run --rm --entrypoint=browser-sync demyx/browsersync --version 2>&1 | sed 's/\r//g')" >> "$DEMYX"/.update_local
+    if [[ ! -f "$DEMYX_IMAGES_FILE" ]]; then
+        docker images --format="{{.Repository}}:{{.Tag}}" | sed '/:<none>/d' | sed 's|:latest||g' > "$DEMYX_IMAGES_FILE"
     fi
 
-    if [[ -n "$(docker images demyx/openlitespeed:latest -q)" ]]; then
-        echo "DEMYX_LOCAL_OPENLITESPEED_VERSION=$(docker run --rm --entrypoint=cat demyx/openlitespeed /usr/local/lsws/VERSION 2>&1 | sed 's/\r//g')" >> "$DEMYX"/.update_local
-        echo "DEMYX_LOCAL_OPENLITESPEED_LSPHP_VERSION=$(docker run --rm --entrypoint=bash demyx/openlitespeed -c '/usr/local/lsws/lsphp74/bin/lsphp -v' 2>&1 | head -1 | awk '{print $2}' | sed 's/\r//g')" >> "$DEMYX"/.update_local
+    case "$DEMYX_IMAGES" in
+        cat)
+            cat < "$DEMYX"/.images
+        ;;
+        path)
+            echo "$DEMYX"/.images
+        ;;
+        update)
+            docker images --format="{{.Repository}}:{{.Tag}}" | sed '/:<none>/d' | sed 's|:latest||g' > "$DEMYX_IMAGES_FILE"
+        ;;
+        *)
+            cat <<< "$DEMYX_IMAGES_FILE"
+        ;;
+    esac
+}
     fi
 
     if [[ -n "$(docker images demyx/ssh:latest -q)" ]]; then
