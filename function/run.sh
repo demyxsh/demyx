e391f86e3d4ee769f3a16ee6908281da34d7fb86
Demyx <info@demyx.sh>
2023-09-15 20:10:12 -0700

    demyx_run_table() - After an app has been created, this function outputs a series of information - Includes login link to WP dashboard

--- function/run.sh
+++ function/run.sh
@@ -346,127 +346,66 @@ demyx_run_soon() {
         demyx_error custom "Coming Soon™"
     fi
 }
+#
+#   Output run table.
+#
+demyx_run_table() {
+    demyx_app_env wp "
+        DEMYX_APP_DB_CONTAINER
+        DEMYX_APP_NX_CONTAINER
+        DEMYX_APP_OLS_ADMIN_PASSWORD
+        DEMYX_APP_OLS_ADMIN_USERNAME
+        DEMYX_APP_OLS_LSPHP
+        DEMYX_APP_PHP
+        DEMYX_APP_WP_CONTAINER
+        WORDPRESS_USER
+        WORDPRESS_USER_EMAIL
+        WORDPRESS_USER_PASSWORD
+    "
 
-            demyx_echo 'Importing clone database'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" db import /demyx/clone.sql
+    {
+        if [[ "$DEMYX_APP_TYPE" = wp ]]; then
+            echo "WordPress Login           $(demyx_app_login)"
+            echo "Username                  $WORDPRESS_USER"
+            echo "Password                  $WORDPRESS_USER_PASSWORD"
+            echo "Email                     $WORDPRESS_USER_EMAIL"
+            echo
 
-            demyx_echo 'Creating admin account'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" user create "$WORDPRESS_USER" info@"$DEMYX_APP_DOMAIN" \
-                --role=administrator \
-                --user_pass="$WORDPRESS_USER_PASSWORD"
+            echo "MariaDB Container         $DEMYX_APP_DB_CONTAINER"
 
-            demyx_echo 'Replacing old URLs'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" search-replace "$DEMYX_RUN_CLONE" "$DEMYX_APP_DOMAIN"
+            if [[ "$DEMYX_APP_STACK" = nginx-php || "$DEMYX_APP_STACK" = bedrock ]]; then
+                echo "Nginx Container           $DEMYX_APP_NX_CONTAINER"
+            fi
 
-            demyx_echo 'Configuring permalinks'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" rewrite structure '/%category%/%postname%/'
+            echo "WordPress Container       $DEMYX_APP_WP_CONTAINER"
 
-            demyx_echo 'Removing clone database'
-            demyx_execute docker exec -t "$DEMYX_APP_WP_CONTAINER" rm -f /demyx/clone.sql
-
-            demyx_echo 'Cleaning up'
-            demyx_execute rm -rf "$DEMYX_APP_PATH"/demyx
-        elif [[ -n "$DEMYX_RUN_ARCHIVE" ]]; then
-            demyx_echo 'Creating new wp-config.php'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" config create \
-                --dbhost="$WORDPRESS_DB_HOST" \
-                --dbname="$WORDPRESS_DB_NAME" \
-                --dbuser="$WORDPRESS_DB_USER" \
-                --dbpass="$WORDPRESS_DB_PASSWORD"
-
-            demyx_echo 'Configuring wp-config.php for reverse proxy'
-            demyx_execute docker run -t --rm \
-                --volumes-from="$DEMYX_APP_WP_CONTAINER" \
-                demyx/utilities demyx-proxy
-
-            demyx_echo 'Installing WordPress'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" core install \
-                --url="$DEMYX_RUN_PROTO" \
-                --title="$DEMYX_APP_DOMAIN" \
-                --admin_user="$WORDPRESS_USER" \
-                --admin_password="$WORDPRESS_USER_PASSWORD" \
-                --admin_email="$WORDPRESS_USER_EMAIL" \
-                --skip-email
-
-            demyx_echo 'Importing archive database'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" db import "${DEMYX_RUN_ARCHIVE//./_}".sql
-
-            demyx_echo 'Creating admin account'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" user create "$WORDPRESS_USER" info@"$DEMYX_APP_DOMAIN" \
-                --role=administrator \
-                --user_pass="$WORDPRESS_USER_PASSWORD"
-
-            demyx_echo 'Replacing old URLs'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" search-replace "$DEMYX_RUN_ARCHIVE" "$DEMYX_APP_DOMAIN"
-
-            demyx_echo 'Configuring permalinks'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" rewrite structure '/%category%/%postname%/'
-
-            demyx_echo 'Configuring salts'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" config shuffle-salts
-
-            demyx_echo 'Removing archive database'
-            demyx_execute docker exec -t "$DEMYX_APP_WP_CONTAINER" rm "${DEMYX_RUN_ARCHIVE//./_}".sql
-
-            demyx_echo 'Cleaning up'
-            demyx_execute rm -rf "$DEMYX_BACKUP_WP"/"$DEMYX_APP_DOMAIN"/"$DEMYX_RUN_ARCHIVE"
-        else
-            demyx_echo 'Configuring wp-config.php'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" core install \
-                --url="$DEMYX_RUN_PROTO" \
-                --title="$DEMYX_APP_DOMAIN" \
-                --admin_user="$WORDPRESS_USER" \
-                --admin_password="$WORDPRESS_USER_PASSWORD" \
-                --admin_email="$WORDPRESS_USER_EMAIL" \
-                --skip-email
-
-            demyx_echo 'Configuring permalinks'
-            demyx_execute demyx wp "$DEMYX_APP_DOMAIN" rewrite structure '/%category%/%postname%/'
+            if [[ "$DEMYX_APP_STACK" = ols || "$DEMYX_APP_STACK" = ols-bedrock ]]; then
+                echo "OLS Admin Login           $(demyx_app_proto)://$(demyx_app_domain "$DEMYX_APP_DOMAIN")/demyx/ols/"
+                echo "OLS Admin Username        $DEMYX_APP_OLS_ADMIN_USERNAME"
+                echo "OLS Admin Password        $DEMYX_APP_OLS_ADMIN_PASSWORD"
+            fi
         fi
 
-        if [[ -n "$DEMYX_RUN_CLONE" ]]; then
-            DEMYX_RUN_CLONE_ENV_AUTH_CHECK="$(demyx info "$DEMYX_RUN_CLONE" --filter=DEMYX_APP_AUTH)"
-            DEMYX_RUN_CLONE_ENV_CACHE_CHECK="$(demyx info "$DEMYX_RUN_CLONE" --filter=DEMYX_APP_CACHE)"
-            DEMYX_RUN_CLONE_ENV_WHITELIST_CHECK="$(demyx info "$DEMYX_RUN_CLONE" --filter=DEMYX_APP_IP_WHITELIST)"
+        echo
+        echo "Stack                     $DEMYX_APP_STACK"
 
-            [[ "$DEMYX_RUN_CLONE_ENV_CACHE_CHECK" = true ]] && demyx config "$DEMYX_APP_DOMAIN" --cache && DEMYX_RUN_CACHE=true
-            [[ "$DEMYX_RUN_CLONE_ENV_AUTH_CHECK" = true ]] && demyx config "$DEMYX_APP_DOMAIN" --auth && DEMYX_RUN_AUTH=true
-            [[ "$DEMYX_RUN_CLONE_ENV_WHITELIST_CHECK" != false ]] && demyx config "$DEMYX_APP_DOMAIN" --whitelist="$DEMYX_RUN_WHITELIST" && DEMYX_RUN_WHITELIST="$DEMYX_RUN_CLONE_ENV_WHITELIST_CHECK"
-        else
-            [[ "$DEMYX_RUN_CACHE" = true ]] && demyx config "$DEMYX_APP_DOMAIN" --cache
-            [[ "$DEMYX_RUN_AUTH" = true ]] && demyx config "$DEMYX_APP_DOMAIN" --auth
-            [[ "$DEMYX_RUN_WHITELIST" = true ]] && demyx config "$DEMYX_APP_DOMAIN" --whitelist="$DEMYX_RUN_WHITELIST"
+        # TODO
+        if [[ "$DEMYX_APP_STACK" = nginx-php || "$DEMYX_APP_STACK" = bedrock ]]; then
+            echo "PHP                       $DEMYX_APP_PHP"
+        elif [[ "$DEMYX_APP_STACK" = ols || "$DEMYX_APP_STACK" = ols-bedrock ]]; then
+            echo "LSPHP                     $DEMYX_APP_OLS_LSPHP"
         fi
 
-        demyx refresh "$DEMYX_APP_DOMAIN" --skip-backup
-        demyx config "$DEMYX_APP_DOMAIN" --healthcheck
+        echo "SSL                       $DEMYX_APP_SSL"
+        echo "Basic Auth                $DEMYX_APP_AUTH"
+        echo "Cache                     $DEMYX_APP_CACHE"
+        echo "Whitelist                 $DEMYX_APP_IP_WHITELIST"
+    } > "$DEMYX_RUN_TRANSIENT"
 
-        if [[ "$DEMYX_RUN_TYPE" = html ]]; then
-            DEMYX_RUN_TABLE_TITLE=HTML
-        elif [[ "$DEMYX_RUN_TYPE" = php ]]; then
-            DEMYX_RUN_TABLE_TITLE=PHP
-        else
-            DEMYX_RUN_TABLE_TITLE=WORDPRESS
-        fi
-
-        PRINT_TABLE="DEMYX^ ${DEMYX_RUN_TABLE_TITLE}\n"
-
-        if [[ "$DEMYX_RUN_STACK" = ols || "$DEMYX_RUN_STACK" = ols-bedrock ]]; then
-            PRINT_TABLE+="OPENLITESPEED URL^ ${DEMYX_RUN_PROTO}/demyx/ols/\n"
-            PRINT_TABLE+="OPENLITESPEED USERNAME^ $DEMYX_APP_OLS_ADMIN_USERNAME\n"
-            PRINT_TABLE+="OPENLITESPEED PASSWORD^ $DEMYX_APP_OLS_ADMIN_PASSWORD\n"
-            PRINT_TABLE+="^\n"
-        fi
-
-        PRINT_TABLE+="WORDPRESS URL^ ${DEMYX_RUN_PROTO}/\n"
-        PRINT_TABLE+="WORDPRESS USERNAME^ $WORDPRESS_USER\n"
-        PRINT_TABLE+="WORDPRESS PASSWORD^ $WORDPRESS_USER_PASSWORD\n"
-        PRINT_TABLE+="WORDPRESS EMAIL^ $WORDPRESS_USER_EMAIL\n"
-        PRINT_TABLE+="^\n"
-
-        if [[ "$DEMYX_RUN_STACK" = nginx-php || "$DEMYX_RUN_STACK" = bedrock ]]; then
-            PRINT_TABLE+="NX CONTAINER^ $DEMYX_APP_NX_CONTAINER\n"
-        fi
+    demyx_execute false \
+        "demyx_divider_title \"$DEMYX_RUN\" \"App Credentials\"; \
+            cat < $DEMYX_RUN_TRANSIENT"
+}
 
         PRINT_TABLE+="WP CONTAINER^ $DEMYX_APP_WP_CONTAINER\n"
         PRINT_TABLE+="DB CONTAINER^ $DEMYX_APP_DB_CONTAINER\n"
