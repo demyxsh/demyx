4d787aca070b5571a54964057a0f0927c66aee9b
Demyx <info@demyx.sh>
2023-09-15 20:46:26 -0700

    demyx_wp() - Main function for demyx wp

--- function/wp.sh
+++ function/wp.sh
@@ -1,16 +1,26 @@
 # Demyx
 # https://demyx.sh
 
+#
+#   demyx wp <app> <args>
+#
 demyx_wp() {
-    demyx_app_config
+    DEMYX_ARG_2="${1:-$DEMYX_ARG_2}"
+    [[ -n "$DEMYX_ARG_2" ]] && shift && local DEMYX_WP_ARGS="$*"
 
-    if [[ "$DEMYX_TARGET" = all ]]; then
-        cd "$DEMYX_WP" || exit
-        for i in *
-        do
-            demyx wp "$i" "$@"
-        done
-    elif [[ "$DEMYX_APP_TYPE" = wp ]]; then
+    case "$DEMYX_ARG_2" in
+        all)
+            demyx_wp_all
+        ;;
+        *)
+            if [[ -n "$DEMYX_ARG_2" ]]; then
+                demyx_wp_app "$DEMYX_ARG_2"
+            else
+                demyx_help wp
+            fi
+        ;;
+    esac
+}
 
         if [[ "$DEMYX_APP_STACK" = bedrock || "$DEMYX_APP_STACK" = ols-bedrock ]]; then
             DEMYX_WP_WORKDIR=/demyx/web
