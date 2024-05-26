f0e35a52a2c4ad70b6ef977ca6b77fd23d94ec97
Demyx <info@demyx.sh>
2024-05-26 15:38:05 -0700

    Prep for new exec arguments

--- function/exec.sh
+++ function/exec.sh
@@ -44,40 +44,43 @@ demyx_exec() {
         shift
     done
 
-    if [[ -n "$DEMYX_ARG_2" ]]; then
-        demyx_arg_valid
-        demyx_app_env wp "
-            DEMYX_APP_DB_CONTAINER
-            DEMYX_APP_NX_CONTAINER
-            DEMYX_APP_WP_CONTAINER
-        "
-
-        # If -t flag is passed then TTY only
-        if [[ -n "$DEMYX_EXEC_FLAG_NON_INTERACTIVE" ]]; then
-            DEMYX_EXEC_TTY="-t"
-        else
-            DEMYX_EXEC_TTY="-it"
-        fi
-
-        # Execute as root
-        if [[ "$DEMYX_EXEC_FLAG_ROOT" = true ]]; then
-            DEMYX_EXEC_USER="--user=root"
-        else
-            DEMYX_EXEC_USER="--user=demyx"
-        fi
-
-        if [[ "$DEMYX_EXEC_FLAG_DB" = true ]]; then
-            DEMYX_EXEC_CONTAINER="$DEMYX_APP_DB_CONTAINER"
-        elif [[ "$DEMYX_EXEC_FLAG_NX" = true ]]; then
-            DEMYX_EXEC_CONTAINER="$DEMYX_APP_NX_CONTAINER"
-        else
-            DEMYX_EXEC_CONTAINER="$DEMYX_APP_WP_CONTAINER"
-        fi
-
-        shift
-
-        eval "exec docker exec $DEMYX_EXEC_TTY $DEMYX_EXEC_USER $DEMYX_EXEC_CONTAINER ${*:-bash}"
+    # If -t flag is passed then TTY only
+    if [[ -n "$DEMYX_EXEC_FLAG_NON_INTERACTIVE" ]]; then
+        DEMYX_EXEC_TTY="-t"
     else
-        demyx_help exec
+        DEMYX_EXEC_TTY="-it"
     fi
+
+    # Execute as root
+    if [[ "$DEMYX_EXEC_FLAG_ROOT" = true ]]; then
+        DEMYX_EXEC_USER="--user=root"
+    else
+        DEMYX_EXEC_USER="--user=demyx"
+    fi
+
+        *)
+            if [[ -n "$DEMYX_ARG_2" ]]; then
+                demyx_arg_valid
+                demyx_app_env wp "
+                    DEMYX_APP_DB_CONTAINER
+                    DEMYX_APP_NX_CONTAINER
+                    DEMYX_APP_WP_CONTAINER
+                "
+
+                if [[ "$DEMYX_EXEC_FLAG_DB" = true ]]; then
+                    DEMYX_EXEC_CONTAINER="$DEMYX_APP_DB_CONTAINER"
+                elif [[ "$DEMYX_EXEC_FLAG_NX" = true ]]; then
+                    DEMYX_EXEC_CONTAINER="$DEMYX_APP_NX_CONTAINER"
+                else
+                    DEMYX_EXEC_CONTAINER="$DEMYX_APP_WP_CONTAINER"
+                fi
+
+                shift
+
+                eval "exec docker exec $DEMYX_EXEC_TTY $DEMYX_EXEC_USER $DEMYX_EXEC_CONTAINER ${*:-bash}"
+            else
+                demyx_help exec
+            fi
+        ;;
+    esac
 }
